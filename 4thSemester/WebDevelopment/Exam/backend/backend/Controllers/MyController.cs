755a4a44d1b41e1c708a854e0a9b77c6dc36bf58
Alen <132235388+fragN7@users.noreply.github.com>
2023-06-22 16:50:27 +0300

    Created exam skeleton

---
+++ 4thSemester/WebDevelopment/Exam/backend/backend/Controllers/MyController.cs
@@ -0,0 +1,30 @@
+﻿using backend.Repo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[Route("api")]
+public class MyController : ControllerBase
+{
+    private readonly DatabaseContext _context;   
+    
+    public MyController(DatabaseContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet("login")]
+    [AllowAnonymous]
+    public ActionResult Login(string username)
+    {
+        var user = _context.Users.FirstOrDefault(x => x.Username == username);
+
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        return Ok(user);
+    }
+}
