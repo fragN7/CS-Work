4c717eae4f39f8cad60f1a23da001fd0d7a0c75f
Alen <132235388+fragN7@users.noreply.github.com>
2023-10-20 09:08:12 +0300

    Cheloo

--- 5thSemester/Parallel and Distributed Programming/Lab1/main.cpp
+++ 5thSemester/Parallel and Distributed Programming/Lab1/main.cpp
@@ -2,7 +2,7 @@
 #include <mutex>
 
 struct Account {
-    unsigned balance;
+    unsigned balance{};
     std::mutex mtx;
 };
 
@@ -21,6 +21,26 @@ bool transfer(Account* a, Account* b, unsigned amount) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Account acc1;
+    acc1.balance = 100;
+
+    Account acc2;
+    acc2.balance = 50;
+
+    // Example 1: Successful transfer
+    if (transfer(&acc1, &acc2, 30)) {
+        std::cout << "Transfer successful." << std::endl;
+    } else {
+        std::cout << "Insufficient funds for transfer." << std::endl;
+    }
+
+    // Example 2: Insufficient funds
+    if (transfer(&acc1, &acc2, 1000)) {
+        std::cout << "Transfer successful." << std::endl;
+    } else {
+        std::cout << "Insufficient funds for transfer." << std::endl;
+    }
+
     return 0;
 }
