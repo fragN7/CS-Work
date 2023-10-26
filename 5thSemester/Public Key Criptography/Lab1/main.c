2852fe1d00be93398f4b4cd3857e7e7022b9297f
Alen <132235388+fragN7@users.noreply.github.com>
2023-10-26 18:44:52 +0300

    Commit again

--- 5thSemester/Public Key Criptography/Lab1/main.c
+++ 5thSemester/Public Key Criptography/Lab1/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <string.h>
 
 // Normal subtraction
 int gcdSubtraction(int a, int b) {
+
+    if(a == 0)
+        return b;
+
     while (a != b) {
         if (a > b) {
             a -= b;
@@ -18,7 +21,7 @@ int gcdSubtraction(int a, int b) {
 // Normal division
 int gcdDivision(int a, int b) {
     if (a == 0){
-       return b;
+        return b;
     }
     if (b == 0) {
         return a;
@@ -35,6 +38,10 @@ int gcdDivision(int a, int b) {
 
 // Normal
 int gcdBasic(int a, int b){
+
+    if(a == 0)
+        return b;
+
     int min = 0;
     if(a < b){
         min = a;
@@ -61,8 +68,8 @@ int main() {
     struct timespec start, end;
     double cpu_time_used;
 
-    int pairs[20][2] = {
-            {2 * 11, 3 * 11},
+    long long int pairs[20][2] = {
+            {0, 5},
             {2 * 13, 3 * 13},
             {2 * 17, 3 * 17},
             {2 * 19, 3 * 19},
@@ -75,25 +82,25 @@ int main() {
             {2 * 47, 3 * 47},
             {2 * 53, 3 * 53},
             {2 * 59, 3 * 59},
-            {2 * 61, 3 * 61},
-            {2 * 67, 3 * 67},
-            {2 * 71, 3 * 71},
-            {2 * 73, 3 * 73},
-            {2 * 79, 3 * 79},
-            {2 * 83, 3 * 83},
-            {2 * 89, 3 * 89}
+            {8 * 61, 9 * 61},
+            {187877, 321321},
+            {312312, 883213},
+            {412421, 421414},
+            {321313, 999999},
+            {321311, 493213},
+            {124143, 431266}
     };
 
     printf("Comparative Analysis of GCD Algorithms\n");
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 10; i++) {
         int index = rand() % 20;
 
         int a = pairs[index][0];
         int b = pairs[index][1];
 
         clock_gettime(CLOCK_REALTIME, &start);
-        int result1 = gcdDivision(a, b);
+        int result1 = gcdSubtraction(a, b);
         clock_gettime(CLOCK_REALTIME, &end);
         cpu_time_used = (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / 1000000000.0;
         printf("GCD of %d and %d using normal subtraction algorithm is %d. Time taken: %.9f seconds\n", a, b, result1, cpu_time_used);
