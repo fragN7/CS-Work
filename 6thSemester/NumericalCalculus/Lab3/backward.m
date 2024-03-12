bd5ffb9c9601f7234613b7a0fe6a794c934ff63a
Alen <132235388+fragN7@users.noreply.github.com>
2024-03-12 15:20:38 +0200

    Added sum

---
+++ 6thSemester/NumericalCalculus/Lab3/backward.m
@@ -0,0 +1,8 @@
+﻿function t = backward(f)
+    n = length(f);
+    t = zeros(n);
+    t(:, 1) = f';
+    for k = 2:n
+        t(k:n, k) = diff(t(k-1:n, k-1));
+    end
+end
---
+++ 6thSemester/NumericalCalculus/Lab3/double_nodes.m
@@ -0,0 +1,11 @@
+﻿function[z, t] = double_nodes(x, f, df)
+    n = length(x);
+    z = repelem(x, 2);
+    t = zeros(2 * n);
+    t(:, 1) = repelem(f, 2)';
+    t(1:2:2*n - 1, 2) = df';
+    t(2:2:2*n - 2, 2) = (diff(f)./diff(x))';
+    for k = 3:2*n
+        t(1:2*n-k+1, k) = diff(t(1:2*n-k+2, k-1))./(z(k:2*n) - z(1:2*n-k+1))';
+    end
+end
