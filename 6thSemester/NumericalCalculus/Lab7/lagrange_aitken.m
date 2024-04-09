2b6e529bd83a400f333a4fe3bc3bdefcc1cfa586
Alen <132235388+fragN7@users.noreply.github.com>
2024-04-09 15:38:50 +0300

    ex2

---
+++ 6thSemester/NumericalCalculus/Lab7/lagrange_aitken.m
@@ -0,0 +1,10 @@
+﻿function x = lagrange_aitken(xi, fi, x)
+    n = length(xi);
+    p = zeros(n);
+    for i = 1:n
+        p(i,1) = fi(i);
+        for j = 1:i-1
+            p(i, j+1) = ((x-xi(j))* p(i,j) - (x- xj(i)) * p(j,j)) / (xi(i)-xi(j));
+        end
+    end
+endfunction
---
+++ 6thSemester/NumericalCalculus/Lab7/lagrange_newton.m
@@ -0,0 +1,6 @@
+﻿function f = lagrange_newton(xi, d, x)
+    for k = 1:length(x)
+        y = x(k) - xi;
+        f(k) = d(1, :) * [1, cumprod(y(1:length(y)-1))]';
+    end
+endfunction
