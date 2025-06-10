defe553a05d07e29d53059351225ab65c6789af9
Alen <132235388+fragN7@users.noreply.github.com>
2025-06-10 20:03:32 +0300

    Frontadded

--- 6thSemester/BachelorThesis/Thesis/backend/backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ 6thSemester/BachelorThesis/Thesis/backend/backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775a5ace40ab631a82126175547f9558fb83b787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5379fd13f2e6d4fa9876ddc7d82e8cb0ffc3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
